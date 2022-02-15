638f14b4bc84e461923f83dc34b77b8f3d67a294
Fadhil Prawira <fadhilprawira87@gmail.com>
2022-02-15 10:33:05 +0700

    tambah komentar, edit supaya rapi dan mudah dibaca

--- matkul-maker.cmd
+++ matkul-maker.cmd
@@ -1,1 +1,6 @@
-for /F "usebackq delims=" %%i in (list-matkul.txt) do mkdir "%%i" 
+@echo off
+SETLOCAL DisableDelayedExpansion
+Rem Baca file list-matkul.txt lalu membuat folder bernama mata kuliah dari list yang sudah dibuat
+for /F "usebackq delims=" %%i in (list-matkul.txt) do (
+	mkdir "%%i" 
+)
